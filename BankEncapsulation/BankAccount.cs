fcb859f380f9070d1d688819a7db76bb5d268c32
“Noah <urbannoah@yahoo.com>
2024-06-12 11:55:43 -0400

    rough draft complete Bank Encapsulation

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class BankAccount
+{
+	private double _balance = 0;
+
+	public BankAccount()
+	{
+		
+	}
+
+	public void Deposit(double amount)
+	{
+		_balance += amount;
+	}
+
+	public void GetBalance()
+	{
+		Console.WriteLine($"Balance: {_balance}");
+	}
+}
+
+
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace BankEncapsulation
+﻿using System;
+
+namespace BankEncapsulation
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount Noah = new BankAccount();
+
+            Noah.GetBalance();
+
+            Console.WriteLine();
+            Console.WriteLine("Please enter your deposit amount...");
+            Noah.Deposit(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to see your account balance");
+            Console.ReadKey();
+
+            Console.WriteLine();
+            Noah.GetBalance();
         }
     }
 }
